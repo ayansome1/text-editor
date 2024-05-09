b125232f3a70da86355340d026251ff36b7aaa7d
ayansome1 <ayansome1@gmail.com>
2024-05-09 11:29:07 +0530

    drag and drop support

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import styles from '../styles/TextEditor.module.scss';
 
 function App() {
-  const [text, setText] = useState(
-    'Select some text and toggle bold or italic formatting.'
-  );
-
-  const [paras, setParas] = useState([true]);
+  const [paras, setParas] = useState([{ id: uuidv4() }]);
   const [focusedParaIndex, setFocusedParaIndex] = useState(null);
 
   const elRefs = useRef([]);
@@ -14,8 +12,6 @@ function App() {
   // Set cursor focus on newly created paragraph
   useEffect(() => {
     focusOnPara();
-    // const size = elRefs.current.length;
-    // elRefs.current[size - 1].focus();
   }, [paras]);
 
   const focusOnPara = () => {
@@ -32,77 +28,83 @@ function App() {
   };
 
   const handleColorChange = () => {
-    // document.execCommand('styleWithCSS', false, true);
     document.execCommand('foreColor', false, 'green');
   };
 
   const handleTextSelect = () => {
     const selectedText = window.getSelection().toString();
     console.log(selectedText);
-    // setText(selectedText);
   };
 
   const createLink = () => {
     document.execCommand('createLink', false, 'www.google.com');
   };
 
-  const handleInput = (e) => {
-    console.log(e.target.innerText);
-    // setText(e.target.innerText);
-  };
-
   const handleKeyDown = (e) => {
-    // event.key === 'Enter'
-    if (e.keyCode === 13) {
+    if (e.key === 'Enter') {
       e.preventDefault();
-      const size = elRefs.current.length;
-      console.log(size + ' ' + focusedParaIndex);
       if (focusedParaIndex == paras.length - 1) {
-        setParas((arr) => [...arr, true]);
+        setParas((arr) => [...arr, { id: uuidv4() }]);
       } else {
         elRefs.current[focusedParaIndex + 1].focus();
       }
-      // setParas((arr) => [...arr, true]);
     }
+  };
+
+  const swapBoxes = (fromIndex, toIndex) => {
+    const tempHtml = elRefs.current[fromIndex].innerHTML;
+    elRefs.current[fromIndex].innerHTML = elRefs.current[toIndex].innerHTML;
+    elRefs.current[toIndex].innerHTML = tempHtml;
+  };
+
+  const handleDragStart = (index) => (event) => {
+    event.dataTransfer.setData('dragContent', index);
+  };
+
+  const handleDragOver = () => (event) => {
+    event.preventDefault();
+    return false;
+  };
+
+  const handleDrop = (index) => (event) => {
+    event.preventDefault();
+    const fromIndex = event.dataTransfer.getData('dragContent');
+    swapBoxes(fromIndex, index);
+    return false;
   };
 
   return (
     <div>
-      {paras.map((val, index) => {
-        return (
-          <div
-            // key={index}
-            onFocus={() => {
-              console.log(index);
-              setFocusedParaIndex(index);
-            }}
-            contentEditable
-            ref={(el) => (elRefs.current[index] = el)}
-            // ref={(el) => (elRefs.current = [...elRefs.current, el])}
-            // tabIndex={1}
-            autoFocus
-            className={styles.paragraph}
-            // style={{
-            //   border: '1px solid #ccc',
-            //   padding: '10px',
-            //   minHeight: '100px',
-            // }}
-            onDoubleClick={handleTextSelect}
-            // onSelect={handleTextSelect}
-            onInput={handleInput}
-            onKeyDown={handleKeyDown}
-          >
-            {/* <div>{text}</div> */}
-          </div>
-        );
-      })}
-
       <button onClick={handleBoldClick}>Bold</button>
       <button onClick={handleItalicClick}>Italic</button>
       <button onClick={createLink}>Create link</button>
 
       <button onClick={handleColorChange}>Change color</button>
       <button>{focusedParaIndex}</button>
+      <div className={styles.paragraphWrapper}>
+        {paras.map((val, index) => {
+          return (
+            <div
+              key={val.id}
+              onFocus={() => {
+                setFocusedParaIndex(index);
+              }}
+              contentEditable
+              ref={(el) => (elRefs.current[index] = el)}
+              autoFocus
+              className={styles.paragraph}
+              onDoubleClick={handleTextSelect}
+              // onSelect={handleTextSelect}
+              // onInput={handleInput}
+              onKeyDown={handleKeyDown}
+              draggable={true}
+              onDragStart={handleDragStart(index)}
+              onDragOver={handleDragOver()}
+              onDrop={handleDrop(index)}
+            ></div>
+          );
+        })}
+      </div>
     </div>
   );
 }
